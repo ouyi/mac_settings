224d50dc4d0c0c9a6b342bd01e4ccede09d07d19
Yi Ou <ouyi@gmx.de>
2018-05-02 21:25:46 +0200

    Add vscode user settings

--- update_settings.sh
+++ update_settings.sh
@@ -2,12 +2,14 @@
 
 set -e
 
-cd settings
+pushd settings
 
 for f in bash_profile bashrc gitconfig vimrc; do
     cp ~/.$f $f
 done
 
+cp "$HOME/Library/Application Support/Code/User/settings.json" vscode_user_settings.json
+
 echo '# brew leaves > brew_leaves' > brew_leaves && brew leaves >> brew_leaves
 
-git status -s
+popd && git status -s
